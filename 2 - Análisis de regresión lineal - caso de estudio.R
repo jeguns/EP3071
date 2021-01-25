c81dbe06392db0102aafa1a8c39321bb0bc36b9d
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-01-25 09:45:26 -0500

    Actualización 25/01

--- 2 - Análisis de regresión lineal - caso de estudio.R
+++ 2 - Análisis de regresión lineal - caso de estudio.R
@@ -10,25 +10,32 @@ library(olsrr)    # para las funciones de selección de variables
 library(nortest)  # para la función ad.test
 library(lmtest)   # para la función dwtest
 library(car)      # para la función vif
+library(dplyr)    # para usar el operador pipe %>% 
 
 # Lectura de datos
 
-datosreg = read.table("CasoRegresionLineal.txt", header = T)
+datosreg = read.table("CasoRegresionLineal.txt", header = TRUE)
 
 # Análisis exploratorio 
 
+datosreg$AdultoMayor = as.factor(datosreg$AdultoMayor)
+datosreg$Secundaria  = as.factor(datosreg$Secundaria)
+
 summary(datosreg)
 
-cor(datosreg)
+# Análisis exploratorio cuantitativo
 
-corrplot(cor(datosreg))
-corrplot(cor(datosreg),addCoef.col = "black")
+datosreg2 = datosreg[,-c(4,6)]
+cor(datosreg2)
+corrplot(cor(datosreg2))
+corrplot(cor(datosreg2),addCoef.col = "black")
+heatmap(x = cor(datosreg2),symm = TRUE)
+chart.Correlation(datosreg2, histogram=TRUE, pch=19)
 
-heatmap(x = cor(datosreg),symm = TRUE)
-
-chart.Correlation(datosreg, histogram=TRUE, pch=19)
-
+# Análisis exploratorio cuantitativo y cualitativo
+boxplot(datosreg$Area)
 boxplot(datosreg$Area~datosreg$Secundaria)
+boxplot(datosreg$Area~datosreg$AdultoMayor)
 
 datosreg %>% 
   ggplot(aes(x=as.factor(Secundaria),y=Area)) +
@@ -45,9 +52,9 @@ summary(modelo)
 
 # Selección de variables
 
-ols_step_forward_p(modelo, details = TRUE)
-ols_step_backward_p(modelo, details = TRUE)
-ols_step_both_p(modelo, details = TRUE)
+ols_step_forward_p(modelo, details = TRUE, penter = 0.05)
+ols_step_backward_p(modelo, details = TRUE, prem = 0.05)
+ols_step_both_p(modelo, details = TRUE, penter = 0.05, prem = 0.05)
 
 ols_step_forward_aic(modelo, details = TRUE)
 ols_step_backward_aic(modelo, details = TRUE)
@@ -55,6 +62,21 @@ ols_step_both_aic(modelo, details = TRUE)
 
 modelof = lm(Area~Ingreso+Familia+AdultoMayor, data = datosreg)
 summary(modelof)
+
+# Area_estimada = 82.69 + 1.15Ingreso + 32.29Familia + 52.19AdultoMayor
+
+# beta0 = 82.69, no tendría sentido su interpretación porque si Familia = 0...
+
+# beta1 = 1.15, el área estimada / promedio se incrementa en 1.15m² cuando
+# el ingreso se incrementa en 1000 soles anuales, manteniendo las
+# demás características / variables constantes
+
+# beta2 = 32.29, el área estimada se incrementa en 32.29 m² cuando 
+# la familia tiene un miembro más, manteniendo las
+# demás características / variables constantes
+
+# beta3 = 52.19, el área estimada cuando hay un adulto mayor (1) en la
+# familia es 52.19 m² mayor que cuando no lo hay (0).
 
 # Verificación de supuestos
 
@@ -81,12 +103,12 @@ vif(modelof)
 
 # Predicción
 
-predict(modelof, data.frame(Ingreso=100,Familia=3,AdultoMayor=1))
-predict(modelof, data.frame(Ingreso=100,Familia=3,AdultoMayor=1),interval="confidence")
-predict(modelof, data.frame(Ingreso=100,Familia=3,AdultoMayor=1),interval="prediction")
+predict(modelof, data.frame(Ingreso=100,Familia=3,AdultoMayor="1"))
+predict(modelof, data.frame(Ingreso=100,Familia=3,AdultoMayor="1"),interval="confidence")
+predict(modelof, data.frame(Ingreso=100,Familia=3,AdultoMayor="1"),interval="prediction")
 
-X = data.frame(Ingreso=c(100,150,200),Familia=c(3,4,3),AdultoMayor=c(0,0,1))
-predict(modelo_final,X)
-predict(modelo_final,X,interval="confidence")
-predict(modelo_final,X,interval="prediction")
-predict(modelo_final,X,interval="prediction",level=0.9)
+X = data.frame(Ingreso=c(100,150,200),Familia=c(3,4,3),AdultoMayor=c("0","0","1"))
+predict(modelof, X)
+predict(modelof, X,interval="confidence")
+predict(modelof, X,interval="prediction")
+predict(modelof, X,interval="prediction",level=0.99)
