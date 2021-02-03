b3118b65d3cfc0d6ecf81d4053ffcb33d6f14654
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-02-03 10:56:29 -0500

    Actualización 03/02

--- 3 - Análisis de regresión logística.R
+++ 3 - Análisis de regresión logística.R
@@ -12,18 +12,32 @@ modelo_nulo = glm(Interes~1, data = datos, family = "binomial")
 library(lmtest)
 TestRV = lrtest(modelo_nulo,modelo_logistico)
 TestRV
+TestRV = lrtest(modelo_logistico,modelo_nulo)
+TestRV
 
 anova(modelo_logistico, test = "Chisq") # Equivalente solo para regresión logística simple
 
 TestRV$LogLik
-1-TestRV$LogLik[2]/TestRV$LogLik[1]
+1-TestRV$LogLik[1]/TestRV$LogLik[2]
 
-predicciones = predict(modelo_logistico, type=c("response"))
+predict(modelo_logistico, data.frame(Edad=25))
+5.77413 - 0.21906*25
+
+predict(modelo_logistico, data.frame(Edad=25), type="response")
+exp(5.77413 - 0.21906*25)/(1+exp(5.77413 - 0.21906*25))
+
+predict(modelo_logistico, data.frame(Edad=53))
+5.77413 - 0.21906*53
+
+predict(modelo_logistico, data.frame(Edad=53),type="response")
+exp(5.77413 - 0.21906*53)/(1+exp(5.77413 - 0.21906*53))
+
+predicciones = predict(modelo_logistico, type="response")
 comparacion  = data.frame(OBS  = datos$Interes,
                           PRED = as.factor(round(predicciones,0)))
 
 library(caret)
-confusionMatrix(comparacion$PRED, comparacion$OBS, positive = "1")
+confusionMatrix(comparacion$OBS,comparacion$PRED,  positive = "1")
 library(pROC)
 rocobj = roc( datos$Interes, predicciones, auc = TRUE, ci = TRUE  )
 plot(rocobj)
